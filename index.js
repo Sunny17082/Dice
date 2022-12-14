f815bded5abc4feedc42eaa9cb057c550196f5d9
Sunny17082 <91121970+Sunny17082@users.noreply.github.com>
2022-12-14 20:13:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var randomNumber1=Math.round(Math.random()*6)+1;
+var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomDiceImage1="images/dice"+randomNumber1+".png";
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomDiceImage1);
-var randomNumber2=Math.round(Math.random()*6)+1;
+var randomNumber2=Math.floor(Math.random()*6)+1;
 var randomDiceImage2="images/dice"+randomNumber2+".png";
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomDiceImage2);
